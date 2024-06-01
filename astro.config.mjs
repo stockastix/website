76aa79353bce73d26420dc46d38ae04dc9126089
swiing <persovince@yahoo.fr>
2024-06-01 18:20:54 +0200

    build: add netlify integration
    See https://docs.astro.build/en/guides/deploy/netlify/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://stockastix.com",
+  output: "server",
+  adapter: netlify(),
 });
