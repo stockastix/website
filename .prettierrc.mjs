65ec03750f47e4adea7ee7a7e032bb9a670a88e5
swiing <persovince@yahoo.fr>
2024-09-20 17:44:57 +0200

    style: reformat spaces

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,13 +3,13 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-    plugins: ['prettier-plugin-astro'],
-    overrides: [
-      {
-        files: '*.astro',
-        options: {
-          parser: 'astro',
-        },
-      },
-    ],
-  };
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ]
+}
